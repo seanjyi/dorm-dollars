19d8c2a3ff2462e7ff48ddea993abd8a67456b29
seanjyi <seanjyi01@gmail.com>
2023-10-28 16:47:15 -0400

    Navbar basic functionality

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -3,26 +3,28 @@ import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 
 const Navbar = () => {
-
     return (
-        <AppBar>
+        <AppBar sx={{ background: 'purple' }}>
             <Toolbar sx={{mx: 4}}>
                     <Box sx={{ flexGrow: 1, display: 'flex'}}>
-                        <Typography variant="h5">
-                            Dorm Dollars
-                        </Typography>
+                        <Button href="/home">
+                            <Typography variant="h5" sx={{ color: 'white'}}>
+                                Dorm Dollars
+                            </Typography>
+                        </Button>
 
-                        <Typography variant="h6" sx={{ml: 4}}>
-                            History
-                        </Typography>
+                        <Button href="/history" sx={{ textTransform: 'none' }}>
+                            <Typography variant="h6" sx={{ ml: 4, color: 'white' }}>
+                                History
+                            </Typography>
+                        </Button>
                     </Box>
 
-                    <Box sx={{ flexGrow: 0, alignItems: 'center' }}>                       
-                        <Button variant="contained" color="error">
-                            Outlined
+                    <Box sx={{ flexGrow: 0 }}> 
+                        <Button href="/login" variant="contained" color="error">
+                            Logout
                         </Button>
                     </Box>
 
