f3c4a5ecec885ee520bfb90d9b03ea63446ffba5
Abel Lu <aluprof4@gmail.com>
2023-10-28 14:17:16 -0400

    backend logic

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import psycopg2
+from psycopg2 import extras
 import os
 from dotenv import load_dotenv
 
@@ -7,27 +8,33 @@ load_dotenv()
 
 
 conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASS")
+    password=os.getenv("DB_PASS"),
+    host=os.getenv("DB_HOST"),
+    sslmode="disable"
 )
+
+cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     if request.method == "GET":
-        return "<p>Hello World<p>"
+        cur.execute("SELECT * from person;")
+        return cur.fetchall()
     elif request.method == "POST":
         return {"message":"Hello World!"}
     
-@app.route("/login/", methods=["GET", "POST"])
+@app.route("/login/", methods=["POST"])
 def login():
-    if request.method == "GET":
-        return "<p>Login page<p>"
-    elif request.method == "POST":
-        return {"message":"Login page"} 
+    if request.method == "POST":
+        data = request.get_json()
+        username = data['username']
+        q = f"SELECT * from person where name = '{username}'"
+        cur.execute(q)
+        return [row for row in cur.fetchall()]
 
 
 
