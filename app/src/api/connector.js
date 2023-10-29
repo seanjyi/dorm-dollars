e8b250db9a6a01a9492c7d97246eb80f38e49e31
Abel Lu <aluprof4@gmail.com>
2023-10-28 22:16:47 -0400

    localhost

--- app/src/api/connector.js
+++ app/src/api/connector.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiPath = 'http://localhost:5000'
+const apiPath = 'http://127.0.0.1:5000'
 const headers = {
     headers: {
         'Content-Type': 'application/json'
